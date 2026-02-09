3699d4e3fcececd80f24f889f7460f134d5773f6
022nichapha <664259022@webmail.npru.ac.th>
2026-02-09 15:37:10 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/finalsmoodloctionProject/", // ต้องตรงกับชื่อ Repo บน GitHub
+  base: "/finalsmoodloction1/", // ต้องตรงกับชื่อ Repo บน GitHub
 });
